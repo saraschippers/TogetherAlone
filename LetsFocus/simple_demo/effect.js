8e58574ac5f60d65d7d4b77844ea7034c01fc366
saraschippers <66113846+saraschippers@users.noreply.github.com>
2020-06-19 17:31:45 +0200

    uploading project

---
+++ LetsFocus/simple_demo/effect.js
@@ -0,0 +1,7 @@
+var seriously = new Seriously();
+
+function setup(){
+
+var src = seriously.source('#partnerVideo1');       //connect seriously with the video ID
+var target = seriously.target('#p5canvas');
+}
