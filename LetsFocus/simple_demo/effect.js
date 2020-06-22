39b1396c2b653cc3db1b25179c3f02e31a20d662
saraschippers <66113846+saraschippers@users.noreply.github.com>
2020-06-22 17:05:58 +0200

    Delete effect.js

--- LetsFocus/simple_demo/effect.js
+++
@@ -1,7 +0,0 @@
-var seriously = new Seriously();
-
-function setup(){
-
-var src = seriously.source('#partnerVideo1');       //connect seriously with the video ID
-var target = seriously.target('#p5canvas');
-}
